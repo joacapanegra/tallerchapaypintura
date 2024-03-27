f7123e2872ac616d95246fb121f60028426f4761
tupa <jcapanegra@gmail.com>
2024-03-27 11:48:49 -0300

    agregados de secciones

---
+++ funciones.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Selecciona todas las imágenes dentro de hover-container
+  var images = document.querySelectorAll(".hover-container img");
+
+  // Añade un evento click a cada imagen
+  images.forEach(function (image) {
+    image.addEventListener("click", function (event) {
+      event.stopPropagation(); // Evita que el evento de clic se propague al contenedor
+
+      // Agrega la clase 'expanded' a la imagen actual
+      this.classList.toggle("expanded");
+
+      // Muestra el botón de volver cuando se amplíe la imagen
+      document.getElementById("volver").style.display = "block";
+    });
+  });
+
+  // Añade un evento click al botón de volver para restablecer la imagen a su tamaño original
+  document.getElementById("volver").addEventListener("click", function () {
+    // Selecciona todas las imágenes expandidas
+    var expandedImages = document.querySelectorAll(
+      ".hover-container img.expanded"
+    );
+
+    // Restablece el tamaño original de todas las imágenes expandidas
+    expandedImages.forEach(function (expandedImage) {
+      expandedImage.classList.remove("expanded");
+    });
+
+    // Oculta el botón de volver
+    this.style.display = "none";
+  });
+});
